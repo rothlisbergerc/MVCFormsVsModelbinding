284b81332a682af1d6c71d7b22ea9d6416c44378
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-23 10:08:06 -0700

    Updated Duck property documentation

--- MVCFormsVsModelbinding/Models/Duck.cs
+++ MVCFormsVsModelbinding/Models/Duck.cs
@@ -5,22 +5,28 @@ using System.Threading.Tasks;
 
 namespace MVCFormsVsModelbinding.Models
 {
+    /// <summary>
+    /// Represents an individual Duck.
+    /// </summary>
     public class Duck
     {
+        /// <summary>
+        /// The unique identifier for the Duck.
+        /// </summary>
         public int DuckId { get; set; }
 
         /// <summary>
-        /// The name of the duck.
+        /// The name of the Duck.
         /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        /// The color(s) of the duck.
+        /// The color(s) of the Duck.
         /// </summary>
         public string Color { get; set; }
 
         /// <summary>
-        /// The duck's favorite thing to say.
+        /// The Duck's favorite thing to say.
         /// </summary>
         public string CatchPhrase { get; set; }
     }
