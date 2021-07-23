ea07fb4f9ed38e659cc04160db4df7cd26c2310e
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-23 10:01:59 -0700

    Add project files.

---
+++ MVCFormsVsModelbinding/Models/Duck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCFormsVsModelbinding.Models
+{
+    public class Duck
+    {
+        public int DuckId { get; set; }
+    }
+}
