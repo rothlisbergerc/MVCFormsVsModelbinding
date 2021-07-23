0fb9545927e95d4d13ba1658636bd43a90aa7b47
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-23 10:44:41 -0700

    Added HttpPost Add action

--- MVCFormsVsModelbinding/Controllers/DuckController.cs
+++ MVCFormsVsModelbinding/Controllers/DuckController.cs
@@ -1,19 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MVCFormsVsModelbinding.Models;
 
 namespace MVCFormsVsModelbinding.Controllers
 {
     public class DuckController : Controller
     {
-        /// <summary>
-        /// Adds a Duck object to the database
-        /// </summary>
-        /// <returns></returns>
+        [HttpGet]
         public IActionResult Add()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(IFormCollection form)
+        {
+            // Instantiate a new Duck object:
+            Duck du = new Duck();
+
+            // Populate each Duck property:
+            du.Name = form["name"];
+            du.Color = form["color"];
+            du.CatchPhrase = form["catchphrase"];
+
+            // Add to database
+
+            ViewData["Added"] = du.Name + "was added with an ID of 8008";
+
             return View();
         }
     }
