8cd0abbb83d231b3c861ab403349c2478dab8402
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-23 10:06:05 -0700

    Created properties  and property documentation in the Duck class

--- MVCFormsVsModelbinding/Models/Duck.cs
+++ MVCFormsVsModelbinding/Models/Duck.cs
@@ -8,5 +8,20 @@ namespace MVCFormsVsModelbinding.Models
     public class Duck
     {
         public int DuckId { get; set; }
+
+        /// <summary>
+        /// The name of the duck.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The color(s) of the duck.
+        /// </summary>
+        public string Color { get; set; }
+
+        /// <summary>
+        /// The duck's favorite thing to say.
+        /// </summary>
+        public string CatchPhrase { get; set; }
     }
 }
