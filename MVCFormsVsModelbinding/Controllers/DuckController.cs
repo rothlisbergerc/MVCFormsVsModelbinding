0e2324a4049353fb0c9231791e6b8ec0f09e7ba9
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-23 10:15:46 -0700

    Created DuckController

---
+++ MVCFormsVsModelbinding/Controllers/DuckController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCFormsVsModelbinding.Controllers
+{
+    public class DuckController : Controller
+    {
+        /// <summary>
+        /// Adds a Duck object
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
