7157e829da4884a4bbf7d65e0d9ba9dc6b6d4aea
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-23 11:20:17 -0700

    Added data annotations to Duck class properties

--- MVCFormsVsModelbinding/Models/Duck.cs
+++ MVCFormsVsModelbinding/Models/Duck.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCFormsVsModelbinding.Models
 {
@@ -18,16 +19,22 @@ namespace MVCFormsVsModelbinding.Models
         /// <summary>
         /// The name of the Duck.
         /// </summary>
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Your duck must have a name")]
         public string Name { get; set; }
 
         /// <summary>
         /// The color(s) of the Duck.
         /// </summary>
+        [Display(Name = "Color")]
+        [Required(ErrorMessage = "Your duck must have a color")]
         public string Color { get; set; }
 
         /// <summary>
         /// The Duck's favorite thing to say.
         /// </summary>
+        [Display(Name = "Catchphrase")]
+        [Required(ErrorMessage = "Your duck must have a catchphrase")]
         public string CatchPhrase { get; set; }
     }
 }
